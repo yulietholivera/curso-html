2a01cf0a1ede14552f4472a270e05a664b45f86b
Heidy <yulietholivera13@gmail.com>
2024-09-13 09:40:17 -0500

    153. Uso básico de var, let y const en JavaScript

--- JavaScript/Leccion02/02-01- TiposDatosJavaScript.js
+++ JavaScript/Leccion02/02-01- TiposDatosJavaScript.js
@@ -1,17 +1,6 @@
-var nombre = 'Juan';
-var apellido =  'Perez';
+let nombre;
+nombre ="Juan";
+console.log( nombre );
 
-var nombreCompleto = nombre + ' ' + apellido;
-console.log(nombreCompleto);
-
-var nombreCompleto2='Carlos' + ' ' + 'Lara ';
-console.log(nombreCompleto2)
-
-var x = nombre + 2 + 4;
-console.log(x);
-
-x = nombre + (2 + 4);
-console.log(x);
-
-x = 2 + 4 + nombre;
-console.log(x);
+const apellido ="Perez"
+apellido ="Lara";
