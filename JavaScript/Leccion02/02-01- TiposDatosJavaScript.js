c11a01824970951409a97787a7ff113a9b75af5b
Heidy <yulietholivera13@gmail.com>
2024-09-13 07:50:47 -0500

    151. Concatenación de Cadenas en JavaScript

--- JavaScript/Leccion02/02-01- TiposDatosJavaScript.js
+++ JavaScript/Leccion02/02-01- TiposDatosJavaScript.js
@@ -1,63 +1,17 @@
-/*
-Ejemplos de
- tipos de datos 
- en JavaScript
-*/
-//Tipo de dato strings
-var nombre = "Carlos";
-console.log(typeof nombre)
+var nombre = 'Juan';
+var apellido ='Perez';
 
-nombre =10.5;
-console. log(typeof nombre);
+var nombreCompleto = nombre + ' ' + apellido;
+console.log(nombreCompleto);
 
-//Tipo  de dato numerico
-var numero = 1000;
-console.log(numero);
+var nombreCompleto2='Carlos' + ' ' + 'Lara ';
+console.log(nombreCompleto2)
 
-//Tipo de dato object
-var objeto = {
-    nombre: "Juan",
-    apellido: "Perez",
-    telefono: "320998887"
-};
+var x = nombre + 2 + 4;
+console.log(x);
 
-console.log(typeof objeto); 
+x = nombre + (2 + 4);
+console.log(x);
 
-//TIpode dato boolean (true,false)
-var bandera =  false;
-console.log( typeof bandera);
-
-//Tipo de dato function
-function miFuncion(){}
-console.log(typeof miFuncion);
-
-//Tipo de dato Symbol
-var simbolo = Symbol("mi simbolo");
-console.log(typeof simbolo);
-
-//Tipo clase es una function
-class Persona{
-
-    constructor(nombre, apellido){
-        this.nombre = nombre;
-        this.apellido = apellido;
-    }
-}
-console.log(typeof Persona);
-
-//Tipo undefined
-var x;
-console.log(typeof x);
-
-
-//null = ausencia de valor 
-var y = null;
-console.log(typeof y); Object
-
-var autos = ['BMW', 'Audi', 'Volvo'];
-console.log(autos);
-console.log(typeof autos);
-
-var z = '';
-console.log(z);
-console.log(typeof z);
+x = 2 + 4 + nombre;
+console.log(x);
