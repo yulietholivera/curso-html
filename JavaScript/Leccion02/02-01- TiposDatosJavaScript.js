8a9d46685ef07e46a4933496b53d5c2dc2ef130f
Heidy <yulietholivera13@gmail.com>
2024-09-13 07:22:18 -0500

    150. Tipo de Dato Array y Empty String en JavaScript

--- JavaScript/Leccion02/02-01- TiposDatosJavaScript.js
+++ JavaScript/Leccion02/02-01- TiposDatosJavaScript.js
@@ -48,8 +48,16 @@ console.log(typeof Persona);
 //Tipo undefined
 var x;
 console.log(typeof x);
-x =
+
 
 //null = ausencia de valor 
 var y = null;
-console.log(typeof y);
+console.log(typeof y); Object
+
+var autos = ['BMW', 'Audi', 'Volvo'];
+console.log(autos);
+console.log(typeof autos);
+
+var z = '';
+console.log(z);
+console.log(typeof z);
